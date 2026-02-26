5728675b32bf02d1f51ce330a8685622bd0c9733
AcaciaDevops <devops@acaciagreentechnologies.co.uk>
2026-02-26 16:23:30 +0000

    Update hvac.js

--- src/routes/hvac.js
+++ src/routes/hvac.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const hvacController = require('../controllers/hvacController');
+const authorize = require('../middlewares/authorize');
 
 router.get('/', hvacController.getAllHVACs);
 router.get('/:id', hvacController.getHVACById);
-router.post('/', hvacController.createHVAC);
-router.put('/:id', hvacController.updateHVAC);
-router.delete('/:id', hvacController.deleteHVAC);
-router.post("/:id/control", hvacController.controlHVAC);
+router.post('/',  authorize('Administrator','Portfolio Manager','Property Manager'), hvacController.createHVAC);
+router.put('/:id',  authorize('Administrator','Portfolio Manager','Property Manager'),  hvacController.updateHVAC);
+router.delete('/:id',  authorize('Administrator','Portfolio Manager','Property Manager'),  hvacController.deleteHVAC);
+router.post("/:id/control",  authorize('Administrator','Portfolio Manager','Property Manager'),hvacController.controlHVAC);
 
 module.exports = router;
